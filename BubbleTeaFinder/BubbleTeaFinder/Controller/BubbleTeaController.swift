8d2c6711eb658e59af698cbe3894faeac517bbc1
slemeshaev <lemeshaevs@gmail.com>
2021-03-10 23:27:34 +0300

    files for project

---
+++ BubbleTeaFinder/BubbleTeaFinder/Controller/BubbleTeaController.swift
@@ -0,0 +1,29 @@
+//
+//  BubbleTeaController.swift
+//  BubbleTeaFinder
+//
+//  Created by Станислав Лемешаев on 10.03.2021.
+//
+
+import UIKit
+
+class BubbleTeaController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
