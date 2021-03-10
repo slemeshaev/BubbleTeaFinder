403d65eb8019c7b6af7f98ec4ffa239bbdb8e83d
slemeshaev <lemeshaevs@gmail.com>
2021-03-10 22:58:19 +0300

    files for project

---
+++ BubbleTeaFinder/BubbleTeaFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BubbleTeaFinder
+//
+//  Created by Станислав Лемешаев on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
